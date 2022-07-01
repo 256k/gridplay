cefaa855b77d4a3f10592b52d73fc639c0d055c6
256k <fayek.helmi@gmail.com>
2022-07-01 13:24:14 +0100

    added a reset button K1, cleaned up code, added a startup screen

--- gridplay.lua
+++ gridplay.lua
@@ -1,3 +1,12 @@
+--          "Gridplay"                   
+-- experimenting with animation 
+--            by 256k                 
+-- --------------------------------
+-- K2: 
+-- K3: 
+-- E2: 
+-- E3: manually move frames
+
 local g = grid.connect()
 local glyphs = {}
 local glyphindex = 1
@@ -5,26 +14,46 @@ local glyphindex = 1
 function init() 
   glyphs[glyphindex] = initializePage()
   gridredraw(glyphs[glyphindex])
+  updateScreen(glyphindex)
   redraw()
+  
 end
 
 function updateScreen(val)
   screen.clear()
-  screen.move(20,20)
+  screen.move(10, 10)
+  screen.text("Frame: ")
+  screen.move(40,10)
   screen.text(val)
   screen.update()
 end
 
 function redraw()
+  screen.font_size(8)
   screen.clear()
-  screen.move(10,10)
-  screen.text("test")
+  screen.move(1,10)
+  screen.text("Move E3:")
+  screen.move(1,20)
+  screen.text("to advance between frames")
+  screen.move(1,45)
+  screen.text("press grid buttons:")
+  screen.move(1,55)
+  screen.text("to draw the frame")
   screen.update_default()
 end
 
+function key(n,z) 
+  if n==1 and z==1 then 
+    glyphs = {}
+    glyphindex = 1
+    glyphs[glyphindex] = initializePage()
+    gridredraw(glyphs[glyphindex])
+    updateScreen(glyphindex)
+  end
+end
+
 function enc(n,d)
-  if n==3 then glyphindex = glyphindex + d end
-  if glyphindex < 1 then glyphindex = 1 end
+  if n==3 and glyphindex + d >= 1 then glyphindex = glyphindex + d end
   if glyphs[glyphindex] == nil then glyphs[glyphindex] = initializePage() end 
   
   gridredraw(glyphs[glyphindex])
